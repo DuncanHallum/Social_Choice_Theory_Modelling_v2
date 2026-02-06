6c6def113fbecd0e06ebc09fc6e4ba16d65c61fd
DuncanHallum <rc23386@bristol.ac.uk>
2026-02-06 12:11:56 +0000

    Adjust get_samples to take utility function as input
    - Default utility is negative euclidean distance
    - Also fixed bug with PARTY_ID being to long

--- model/sample_ballots.py
+++ model/sample_ballots.py
@@ -10,7 +10,7 @@ BASE_PATH = Path(__file__).parent.parent
 N_VOTERS = 10 #100000 # Rough size of Bristol constituency
 PARTIES = ["Cons", "Lab", "LibDem", "Green", "Reform"]
 EMISSION_DIST = [0.2, 0.23, 0.13, 0.13, 0.31] # from ipsos voting intention opinion poll
-PARTY_IDS = [1101, 1102, 1104, 1105, 1106, 1107, 1110]
+PARTY_IDS = [1101, 1102, 1104, 1107, 1110]
 ELECTION_YEAR = 2024
 ATTRIBUTES = ["lrecon", "galtan"]
 
@@ -55,22 +55,43 @@ def fit_gmm(means, PARTIES, EMISSION_DIST, voter_variance=1.0):
 
 
 # GENERATE SAMPLES
-def get_samples(gmm: GaussianMixture, means, n_voters: int, utility):
-    parties_and_means = [[i, means[i]] for i in range(len(means))] #party index/id & mean
+import math
+from sklearn.mixture import GaussianMixture
+
+def get_samples(gmm: GaussianMixture, means, n_voters: int, utility_fn=None):
+    """
+    Sample voters and produce preference ballots.
+
+    utility_fn(point, party_mean)
+
+    Default utility = negative Euclidean distance (current behavior).
+    """
+
+    parties_and_means = [[i, means[i]] for i in range(len(means))]
     sample_points, sample_cluster_labels = gmm.sample(n_samples=n_voters)
     sample_ballots = []
 
-    def distance(point, index_and_mean):
-        return math.sqrt((point[0]-index_and_mean[1][0])**2 + (point[1]-index_and_mean[1][1])**2)
-        
+    # default utility: negative Euclidean distance
+    if utility_fn is None:
+        def utility_fn(point, party_mean):
+            return -math.sqrt(
+                (point[0] - party_mean[0])**2 +
+                (point[1] - party_mean[1])**2
+            )
+
     for point in sample_points:
-        #Sort index and means by euclidean distance from point (ascending by default)
-        ranking = sorted(parties_and_means, key=lambda x: distance(point, x))
-        #Take just party index/id
+        # sort by utility descending
+        ranking = sorted(
+            parties_and_means,
+            key=lambda x: utility_fn(point, x[1]),
+            reverse=True
+        )
+
         preference_ballot = [x[0] for x in ranking]
         sample_ballots.append(preference_ballot)
 
     return (sample_points, sample_cluster_labels, sample_ballots)
+
 
 # RUN CODE
 if __name__ == "__main__":
